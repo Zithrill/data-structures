89a61b8ba3cbd8af69e6fc96f143f09116caeb11
Zithrill <jacob.gribschaw@gmail.com>
2014-07-25 11:55:01 -0700

    Finished HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,39 +5,54 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var storageNode = {k:v};
+  var storageNode = [k,v];
   if(Array.isArray(this._storage.get(i))){
     this._storage.get(i).push(storageNode);
   }else {
     this._storage.set(i, [storageNode]);
   }
 };
-
+//this.storage.get(i) => [[key,val],["food", pig],["drink", water]];
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   if(Array.isArray(this._storage.get(i))){
     for(var j = 0; j < this._storage.get(i).length; j++ ){
-      if(this._storage.get(i)[j][k]){
-        return this._storage.get(i)[j][k];
+      if(this._storage.get(i)[j][0] === k){
+        return this._storage.get(i)[j][1];
       }
     }
-  };
-
+  }
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var result = this._storage.get(i);
-  this._storage.set(i, null);
-  return result;
+  if(Array.isArray(this._storage.get(i))) {
+    for(var j = 0; j < this._storage.get(i).length; j++) {
+      if(this._storage.get(i)[j][0] === k) {
+        return this._storage.get(i).splice(j, 1, [k, null]);
+      }
+    }
+  }
 };
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+// /*
+//  * Complexity: What is the time complexity of the above functions?
+//  */
+// // var someCollection = new MakeLimitedArray(8);
 
 
+// //var stack = new HashTable();
+// //  creates new object
+// //stack.insert('food', bacon);
+// // var storage = [];
+// / storage.length = 3
+// [0,[0,1]]
+// //stack._storage = {
+// //..
+// //stack._storage.set(hash, [storageNode])
+// var storage = [[]]
+// //}
 //  var makeLimitedArray = function(limit){
 //    var storage = [];
 
@@ -80,4 +95,5 @@ HashTable.prototype.remove = function(k){
 // //get value
 // //send value to getIndexBelowMaxForKey for converson to array index
 // //send index and value to limitedArray.set
+// //
 // //
