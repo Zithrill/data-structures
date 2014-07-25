7db5b00bf63ab229e06e8e5645a68c706db11def
Zithrill <jacob.gribschaw@gmail.com>
2014-07-24 23:18:33 -0700

    working on hashtable

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,59 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(!this._storage[item]){
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item){
+  return this._storage[item] === item;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
+
+
+// var myRevealingModule = (function () {
+//   var privateVar = "Ben Cherry",
+//       publicVar  = "Hey there!";
+
+//   function privateFunction() {
+//       console.log( "Name:" + privateVar );
+//   }
+
+//   function publicSetName( strName ) {
+//       privateVar = strName;
+//   }
+
+
+//   // Reveal public pointers to
+//   // private functions and properties
+
+//   return {
+//       setName: publicSetName,
+//       greeting: publicVar
+//   };
+
+// })();
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// var set0 = makeSet();
+// var set1 = makeSet();
+
+// set = {
+//   _storage: undefined;
+//   add: function() {},
+//   contains: function() {},
+//   remove: function() {}
+// };
