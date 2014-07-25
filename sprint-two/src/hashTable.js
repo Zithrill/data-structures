a5d9c07464fe33a718bc685b2f44b936a7c7f632
Zithrill <jacob.gribschaw@gmail.com>
2014-07-24 23:44:24 -0700

    night save

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,13 +5,24 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log(i);
-  this._storage.set(i, v);
+  var storageNode = {k:v};
+  if(Array.isArray(this._storage.get(i))){
+    this._storage.get(i).push(storageNode);
+  }else {
+    this._storage.set(i, [storageNode]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  if(Array.isArray(this._storage.get(i))){
+    for(var j = 0; j < this._storage.get(i).length; j++ ){
+      if(this._storage.get(i)[j][k]){
+        return this._storage.get(i)[j][k];
+      }
+    }
+  };
+
 };
 
 HashTable.prototype.remove = function(k){
