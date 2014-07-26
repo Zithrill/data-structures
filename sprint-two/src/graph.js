197083d0dd91d0b1f85bacd6dbbb2a3b07010e16
Zithrill <jacob.gribschaw@gmail.com>
2014-07-26 13:38:55 -0700

    Finished all prompts

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,5 +1,6 @@
 var Graph = function(){
-  this.edges = {};
+  this.master = {};
+  this.edges = null;
   this.value = null;
 };
 
@@ -9,41 +10,52 @@ Graph.prototype.addNode = function(newNode, toNode){
   var node = new Graph();
   node.value = newNode;
   // place link to new node into an array to refrence later
-  if(this.edges.length === 1 ){
-    node.edges = this.edges;
-    //addEdge()
+  node.edges = {};
+  this.master[newNode] = node;
+  var masterKeys =Object.keys(this.master);
+  // if this is the first node created its ok to be on its own
+  if(masterKeys.length === 1) {
+    this.edges = {};
+    this.edges[newNode] = node;
   }
-  this.edges[newNode] = node;
-  // if toNode provided set edge on newNode and toNode
-  var nodeToModify = this.edges[toNode];
-  if(nodeToModify){
-    this.addEdge(node, nodeToModify);
+  else if(masterKeys.length === 2){
+    var firstNode = masterKeys[0];
+    this.addEdge(newNode, firstNode );
+  }
+  //if node 3+ and toNode is defined and exists link newNode to toNode
+  else if(masterKeys.length > 2 && toNode && this.contains(toNode)) {
+    this.addEdge(newNode, toNode);
+  }
+  //if this is node #3+ and toNode dose NOT exist throw out this node
+  else{
+    delete node;
+    delete this.master[newNode];
   }
 };
 
 Graph.prototype.contains = function(node){
   //check our refrence array to see if a node exists
     //return true or false
-  return this.edges[node] ? true : false;
-  // return this.edges[node] && true;
+  return this.master[node] ? true : false;
 };
 
 Graph.prototype.removeNode = function(node){
   //lookup node location using refrence array
-  var nodeToModify = this.edges[node];
-  if(nodeToModify){
-        //store node edges
-    var edgesToRemove = nodeToModify.edges;
+  var nodeToDelete = this.master[node];
+  if(nodeToDelete){
+    //store node edges
+    var edgesToRemove = nodeToDelete.edges;
     //for each edge visit node and break link with node being removed
     for(var i = 0; i < edgesToRemove.length; i++){
-      delete nodeToModify.edges[i].edges[node];
+      delete nodeToDelete.edges[i].edges[node];
       //Check if severed node will be orphaned
         //delete node if orphaned
-      if(nodeToModify.edges[i].edges.length === 0){
-        delete nodeToModify.edges[i];
+      if(nodeToDelete.edges[i].edges.length === 0){
+        delete nodeToDelete.edges[i];
       }
     }
     delete this.edges[node];
+    delete this.master[node];
   }
 
 
@@ -51,30 +63,26 @@ Graph.prototype.removeNode = function(node){
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
-  debugger;
-  //lookup fromNode in refrence array
-    //store location
-  //lookup toNode in refrence array
-    //store location
-  //check edges in fromNode for link to toNode
+  return this.master[fromNode].edges[toNode] ? true : false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-  //lookup fromNode in refrence array
-    //store location
-  //lookup toNode in refrence array
-    //store location
-  //add edge to fromNode
-  //add edge to toNode
+  this.master[fromNode].edges[toNode] = this.master[toNode];
+  this.master[toNode].edges[fromNode] = this.master[fromNode];
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
-  //lookup fromNode in refrence array
-    //store location
-  //lookup toNode in refrence array
-    //store location
-  //delete edge from fromNode
-  //delete edge from toNode
+  var startingNode = this.master[fromNode];
+  var endingNode = this.master[toNode];
+
+  delete startingNode.edges[toNode];
+  delete endingNode.edges[fromNode];
+  if(Object.keys(startingNode.edges).length === 0){
+    this.removeNode(fromNode);
+  }
+  if(Object.keys(endingNode.edges).length === 0){
+    this.removeNode(toNode);
+  }
 };
 
 /*
